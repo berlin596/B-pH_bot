a868c1de8a5362c05e588342a6aafcdc03c3934d
berlin596 <76534483+berlin596@users.noreply.github.com>
2021-03-03 14:38:54 -0500

    Update adafruit button test.py

--- adafruit button test.py
+++ adafruit button test.py
@@ -1,5 +1,5 @@
 from Adafruit_IO import Client, Feed
-from time import sleep
+import time
 from adafruit_motorkit import MotorKit
 from atlas_i2c import atlas_i2c
 
@@ -14,43 +14,57 @@ slider = aio.feeds('berlin-dosage')
 pH_high_point = aio.feeds('berlin-ph-high')
 pH_low_point = aio.feeds('berlin-ph-low')
 
-pump = MotorKit (address = 0x61)
+pump = MotorKit(address = 0x61)
 sensor_address = 1
 pH_sensor = atlas_i2c.AtlasI2C()
 pH_sensor.set_i2c_address(sensor_address)
 reading = pH_sensor.query("R", processing_delay=1500)
 
-while True:
-    reading = pH_sensor.query("R",  processing_delay=1500)
-    pH = reading.data
-    pH = round(float(pH.decode("utf-8")),1)
-    print(pH)
-    toggle_value = aio.receive(toggle.key)
-    mode = toggle_value.value
-    slider_value = aio.receive(slider.key)
-    volume = slider_value.value
-    
-    if mode == "Manual":
-        button_value = aio.receive(pump_button.key)
-        result = button_value.value
-        if result == "1":
-            pump.motor1.throttle = 1.0
-            sleep(int(volume))
-            pump.motor1.throttle = 0
-    elif mode == "Auto":
-        low_point = aio.receive(pH_low_point.key)
-        low_point_value=low_point.value
-        if pH < float(low_point_value):
-           pump.motor1.throttle = 1.0
-           sleep(float(volume))
-           pump.motor1.throttle = 0
-    sleep(0.5)
-    
+update_reading = 10.0
+reading_timer = time.time()
 
-           #When it is on auto activate the pH at a certain point
+
+while True:
+    time_now = time.time()
+    
+    if time_now - reading_timer >= update_reading:
+        reading = pH_sensor.query("R",  processing_delay=1500)
+        pH = reading.data
+        pH = round(float(pH.decode("utf-8")),1)
+        print(pH)
+        toggle_value = aio.receive(toggle.key)
+        mode = toggle_value.value
+        slider_value = aio.receive(slider.key)
+        volume = slider_value.value
+        if mode == "Manual":
+            button_value = aio.receive(pump_button.key)
+            result = button_value.value
+            if result == "1":
+                pump.motor1.throttle = 1.0
+                time.sleep(int(volume))
+                pump.motor1.throttle = 0
+        elif mode == "Auto":
+            low_point = aio.receive(pH_low_point.key)
+            low_point_value= float(low_point.value)
+            high_point = aio.receive(pH_high_point.key)
+            high_point_value= float(high_point.value)
+            if pH < float(low_point_value):
+                pump.motor1.throttle = 1.0
+                time.sleep(float(volume))
+                pump.motor1.throttle = 0
+                if pH > float(high_point_value):
+                    pump.motor1.throttle = 1.0
+                    time.sleep(float(volume))
+                    pump.motor1.throttle = 0
+
+        reading_timer = time.time()
+    time.sleep (0.5)
+
+          
            
 
     
     
     
     
+
